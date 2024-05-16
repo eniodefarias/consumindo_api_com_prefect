5c4e4d4902f0b3af4b9f25773cfaea75fbd50009
enio ricardo de farias <eniodefarias@gmail.com>
2024-05-16 20:01:28 -0300

    fix: ajuste no app, corrigi parametros certos do prefect2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from prefect import task, Flow, Parameter
+from prefect import task, Flow
 import requests
 from datetime import timedelta
 
@@ -12,7 +12,9 @@ def get_pokemon_info(pokemon_name):
     return response.json()
 
 # task para puxar habilidades do mesmo pkmn, com retry e timeout
-@task(max_retries=3, retry_delay=timedelta(seconds=5), timeout=10)
+###  esse decorator só funciona no prefect 1.x
+###     @task(max_retries=3, retry_delay=timedelta(seconds=5), timeout=10)
+###  no prefct2 é diferente, é direto no flow
 def get_pokemon_abilities(pokemon_name):
 
     response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}/abilities',
@@ -22,12 +24,15 @@ def get_pokemon_abilities(pokemon_name):
     return response.json()
 
 # fluxo para puxar as tasks
-with Flow("Fluxo de Informações do Pokémon") as flow:
-    pokemon_name = Parameter('pokemon_name', default='ditto')
-
+@Flow(retries=3, retry_delay_seconds=5)
+def poke_flow(pokemon_name: str = 'ditto'):
+    print(f'c1 - pesquisando pokemon_name={pokemon_name}')
     pokemon_info = get_pokemon_info(pokemon_name)
+    print(f'c2 - pokemon_info={pokemon_info}')
     pokemon_abilities = get_pokemon_abilities(pokemon_name)
+    print(f'c3 - pokemon_abilities={pokemon_abilities}')
 
 # Let's rock
 if __name__ == "__main__":
-    flow.run(parameters={"pokemon_name": "jolteon"})
+    print(f'Executando app:')
+    poke_flow(pokemon_name = 'jolteon')
